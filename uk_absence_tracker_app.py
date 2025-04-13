feae90f3c38e07e9f793825b501e03631388afdf
Z-Hadi <83474175+Z-Hadi@users.noreply.github.com>
2025-04-13 16:36:29 +0100

    Update uk_absence_tracker_app.py

--- uk_absence_tracker_app.py
+++ uk_absence_tracker_app.py
@@ -173,6 +173,26 @@ fig = px.timeline(
     color="Trip",
     hover_data=["Info"]
 )
+
+fig.update_layout(
+    showlegend=True,
+    xaxis_title="",
+    yaxis_title="",
+    height=500,
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1, label="1m", step="month", stepmode="backward"),
+                dict(count=6, label="6m", step="month", stepmode="backward"),
+                dict(count=1, label="YTD", step="year", stepmode="todate"),
+                dict(count=1, label="1y", step="year", stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(visible=True),
+        type="date"
+    )
+)
 fig.update_layout(showlegend=True, xaxis_title="", yaxis_title="", height=400)
 fig.update_yaxes(visible=True)
 st.plotly_chart(fig, use_container_width=True)
